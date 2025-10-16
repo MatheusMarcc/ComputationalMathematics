18d9aa8d9d72c2160e30c9aed10ae68051b96758
Matheus Marc Guichard <matheusmarcguichard@gmai.comm>
2025-10-15 23:26:59 -0300

    correção e sugestão do professor

--- algorithms/List1Exercise9.m
+++ algorithms/List1Exercise9.m
@@ -1,72 +1,69 @@
 function List1Exercise9()
- #inicializando os valores
- xI  = 2.5;
- xU  = 5.5;
- eA = 0.1;
- cont = 0;
- max = 1000;
- tolerancia = 1 * 10^-5;
- #Function that calculates the roots
- [dadosX, dadosY] = calculaValores(xI, xU, eA, cont, max, tolerancia);
- plotaGrafico(dadosX, dadosY);
+  clc;
+  # inicializando os valores
+  xI   = 2.5;
+  xU   = 5.5;
+  cont = 0;
+  max  = 1000;
+  tol = 1e-5;
+  # Function that calculates the roots
+  [dadosX, dadosY, xR, fxR, i] = metodoDaFalsaPosicao(xI, xU, max, tol);
+  printf("Valor da raiz: %.6f", xR);
+  printf("\nValor de Y na raiz: %.6f", fxR);
+  printf("\nQuantidade de iteracoes necessarias: %d\n", i);
+  plotaGrafico(dadosX, dadosY);
+endfunction
 
-end
-
-#Calculate f(X)
+# Calculate f(X)
 function y = f(x)
   y = sin(x/2) .* (x - 9/2) .* (x + 29);
-end
+endfunction
 
-#false position method
- function xr = metodoDaFalsaPosicao(xI, xU)
-  fxI = f(xI);
-  fxU = f(xU);
-  xr = xU - (fxU * (xI - xU)) / (fxI - fxU);
- end
-
-#calculate the error
-function eA = calculaEpsilon(xrNovo, xrVelho)
- eA = abs((xrNovo - xrVelho) / xrNovo) * 100;
-end
-
-#calculate the root
-function [dadosX, dadosY] = calculaValores(xI, xU, eA, cont, max, tolerancia)
-  xR = 0;
-  dadosX = [];
-  dadosY = [];
-  while  eA > tolerancia && cont < max
-    cont = cont + 1;
-    xrVelho = xR;
-    fxI = f(xI);
-    fxU = f(xU);
-    xR  = metodoDaFalsaPosicao(xI, xU);
-    fxR = f(xR);
-    dadosX(end + 1) = xR;
-    dadosY(end + 1) = fxR;
+# False position method
+function [dadosX, dadosY, xR, fxR, i] = metodoDaFalsaPosicao(xI, xU, max, tol)
+  dadosX = zeros(1, max);
+  dadosY = zeros(1, max);
+  xrVelho = inf;
+  for i = 1: max
+   fxI = f(xI);
+   fxU = f(xU);
+   xR = xU - (fxU * (xI - xU)) / (fxI - fxU);
+   fxR = f(xR);
+   dadosX(i) = xR;
+   dadosY(i) = fxR;
     if fxI * fxR < 0
-     xI = xR;
-    else
-     xU = xR;
+       xI = xR;
+     else
+       xU = xR;
+     endif
+    if abs(xrVelho - xR) <= tol
+      dadosX = dadosX(1:i);
+      dadosY = dadosY(1:i); #trunco os vetores
+      break;
     endif
-    eA = calculaEpsilon(xR, xrVelho);
-   end
-    x   = xR;
-    y   = fxR;
-    quant = cont;
- end
+    xrVelho = xR;
+  endfor
 
-#show the graph of the f(x) method and the tangent lines of the false position method
- function grafico = plotaGrafico(dadosX, dadosY)
-    x = 2.5:0.1:5.5;
-    plot(x, f(x));
-    xlabel("Eixo x");
-    ylabel("Eixo y");
-    grid on;
-    title("Teste gráfico 2D");
-    hold on;
-    for i = 2:length(dadosX)
-        plot(dadosX(i-1:i), dadosY(i-1:i));
-        pause(0.1);
-    end
- endfunction
+endfunction
 
+# Show the graph of f(x) and tangent lines of the false position method
+function plotaGrafico(dadosX, dadosY)
+  figure(1);
+  qtdeFramesG = length(dadosX);
+  x = 2.5:0.1:5.5;
+  for cont = 1: qtdeFramesG
+   clf;
+   p1 = plot(x, f(x));
+   hold on;
+   p2 = plot(dadosX(cont), dadosY(cont), 'linewidth', 1, 'color', [0 0 0], 'marker',
+   'o', 'markersize', 10, 'markerfacecolor', [1 1 1]);
+   #hold off;
+   set(gca, 'fontsize', 20);
+   xlabel('xR');
+   ylabel('fxR');
+   legend([p1, p2], {'f(x) avaliada', 'raiz calculada'});
+   title(sprintf('Iteracao %d. Valor da raiz: %.6f', cont, dadosY(cont)));
+   grid on;
+   pause(0.001);
+  endfor
+endfunction
