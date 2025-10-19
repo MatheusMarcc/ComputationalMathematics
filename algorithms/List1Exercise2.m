6b8a0915adb030ef1d2296e13fd0a0e33e7c5e70
Matheus Marc Guichard <matheusmarcguichard@gmai.comm>
2025-10-19 19:44:31 -0300

    Exercise 2, final version

--- algorithms/List1Exercise2.m
+++ algorithms/List1Exercise2.m
@@ -3,18 +3,19 @@ function List1Exercise2() # main
   xU = 1.0;
   max = 1000;
   tol = 1e-5;
-  [dadosX, dadosY] = falsePositionMethod(xI, xU, max, tol);
-  printf('X: %.6f:\n', dadosX(end));
-  printf('Y: %.6f:\n', dadosY(end));
+  [dataX, dataY] = falsePositionMethod(xI, xU, max, tol);
+  printf('X: %.6f:\n', dataX(end));
+  printf('Y: %.6f:\n', dataY(end));
+  plotaGrafico(dataX, dataY, xI, xU);
 end
 
 function y = f(x)
    y = x .^3 + 2 .* x .^2 - 2;
 end
 
-function [dadosX, dadosY] = falsePositionMethod(xI, xU, max, tol)
-  dadosX    = zeros(1, max);
-  dadosY    = zeros(1, max);
+function [dataX, dataY] = falsePositionMethod(xI, xU, max, tol)
+  dataX    = zeros(1, max);
+  dataY    = zeros(1, max);
   xrVelho = inf;
   contI = 0;
   contU = 0;
@@ -26,8 +27,8 @@ function [dadosX, dadosY] = falsePositionMethod(xI, xU, max, tol)
      fxR = f(xr);
      eA  = abs(xr - xrVelho);
      xrVelho = xr;
-     dadosX(i)    = xr;
-     dadosY(i)    = fxR;
+     dataX(i)    = xr;
+     dataY(i)    = fxR;
 
      if fxR * fxI > 0
        xI = xr;
@@ -46,16 +47,45 @@ function [dadosX, dadosY] = falsePositionMethod(xI, xU, max, tol)
      endif
 
     if eA < tol
-     dadosX     = dadosX(1:i);
-     dadosY     = dadosY(1:i);
+     dataX  = dataX(1:i);
+     dataY  = dataY(1:i);
      break
     endif
   endfor
 end
 
-function plotaGrafico(dadosX, dadosY, xI, xU, tol)
+function plotaGrafico(dataX, dataY, xI, xU, tol)
   x = xI : 0.1 : xU;
-  for i = 1 : length(dadosX
+  figure(1);
+  for i = 1 : length(dataX)
+   clf;
+   p1 = plot(x, f(x), 'linewidth', 2, 'color', [0.5 0.2 0.1]);
+   hold on;
+   p2 = plot(dataX(i), dataY(i), 'linewidth', 1, 'color', [0 0 0], 'marker',
+   'o', 'markersize', 5, 'markerfacecolor', [1 1 1]);
+   xlabel(sprintf('xR: %.6f', dataX(i)));
+   ylabel(sprintf('f(xR): %.6f', dataY(i)));
+   title('Grafico de convergencia de f(xR)');
+   legend([p1, p2], {'f(x)', 'valor de f(xr)'});
+   grid on;
+   pause(0.1)
   endfor
+  figure(2);
+  j = 1: length(dataX);
+  pXr = plot(j, dataX, 'r-o', 'linewidth', 2, 'markersize', 2);
+  xlabel('iteracao');
+  ylabel('valor de xR');
+  title('grafico de convergencia de xr');
+  legend(pXr, {'valor de xR ao longo do tempo'});
+  grid on;
+
+  figure(3);
+  pFxr = plot(j, dataY, 'r-o', 'linewidth', 2, 'markersize', 2, 'color', [0 0.5 1]);
+  xlabel('iteracao');
+  ylabel('valor de f(xr)');
+  title('Grafico de convergencia de f(xr)');
+  legend(pFxr, {'valor de f(x) ao longo do tempo'});
+  grid on;
+
 end
 
